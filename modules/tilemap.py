8d490e61a1f030de5778eac23b957a04e958663f
Upton Travers <g3b17rastorguevda@mail.ru>
2026-03-29 11:09:22 +0800

    tilemap: is danger check

--- modules/tilemap.py
+++ modules/tilemap.py
@@ -48,3 +48,12 @@ class TileMap:
             tile_id = self.map_data[row][col]
             return tile_id in self.solid_tiles
         return False
+    
+    def is_danger(self, x, y):
+        col = int(x // self.tile_size)
+        row = int(y // self.tile_size)
+
+        if 0 <= row < self.height and 0 <= col < self.width:
+            tile_id = self.map_data[row][col]
+            return tile_id in self.danger_tiles
+        return False
