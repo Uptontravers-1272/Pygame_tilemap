9ba33eeee2d45234b77b78fb92ae7b50749fa01b
Upton Travers <g3b17rastorguevda@mail.ru>
2026-04-05 09:04:08 +0800

    player: init

---
+++ modules/player.py
@@ -0,0 +1,14 @@
+import pygame as pg
+
+class Player:
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
+        self.width = 28
+        self.height = 44
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.speed = 200
+        self.jump_power = 400
+        self.on_ground = False
+        self.alive = True
